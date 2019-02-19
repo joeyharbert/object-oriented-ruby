9c809862d885289acf99ff88f2f1c64ccc1c341d
joeyharbert <joey.harbert@gmail.com>
2019-02-18 20:47:24 -0600

    add items and print phone info

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+phone = {:color => "space grey", :model => "iPhone XR", :price => 800.00}
+tablet = {:color => "gold", :model => "iPad Pro", :price => 999.99}
+computer = {color: "silver", model: "iMac Retina 2016", price: 2000.00}
+
+p "Phone model: #{phone[:model]}"
+p "Phone color: #{phone[:color]}"
+p "Phone price: #{phone[:price]}"
