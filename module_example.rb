071bc58b4440d683a57ac56b781fa3465b65837f
joeyharbert <joey.harbert@gmail.com>
2019-02-20 19:34:07 -0600

    add module example

---
+++ module_example.rb
@@ -0,0 +1,52 @@
+module Movable
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car
+  include Movable
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  include Movable
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+#driver code
+bike = Bike.new()
+bike.ring_bell
+bike.turn("east")
+p bike
+bike.accelerate
+p bike
+bike.brake
+p bike
+
+car = Car.new()
+car.honk_horn
+car.turn("west")
+p car
+car.accelerate
+p car
+car.brake
+p car
+# / driver code
